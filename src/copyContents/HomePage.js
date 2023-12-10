18aa363208cd11f90b32997d618e26e42401f03f
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-12-10 22:59:12 +0100

    Copy: Content for home page

---
+++ src/copyContents/HomePage.js
@@ -0,0 +1,51 @@
+const homeCopy = {
+    heroIntro: "Grow Your Dream Business Online",
+    heroTitle: `For a Reliable Hosting Service, We Are Your Trusted Partner.`,
+    heroBG: `/images/Home%20Hero%20Section_Ducehost%20website%20hosting%20and%20domain%20name%20purchase.webp`,
+    wcuSectionTitle: `Why Choose Us`,
+    wcuSectionSubTitle: `Performance, Reliability and Support Combined`,
+    wcuList: [
+        {
+            Title: `24/7 Customer Support`,
+            Text: `At Ducehost, we offer a round-the-clock support through various channels like live 
+            chat, Email and Phone. Our knowledgeable support staff ensures your hosting experience is smooth and hassle free`,
+            Img: ``
+        },
+        {
+            Title: `Security First Measure`,
+            Text: `We employ robust security measures including firewalls, DDoS protection and regular malware scans. SSL Certificates come standard to safeguard your data and visitor trust.`,
+            Img: ``
+        },
+        {
+            Title: `Scalability and Flexibility`,
+            Text: `You have the ability to upgrade or downgrade your hosting plans as your website grows or as your needs evolve. Our flexible plans ensures you are never overpaying or underserved`,
+            Img: ``
+        },
+        {
+            Title: `Free Domain Name`,
+            Text: `At Ducehost, we provide a complimentary ‘.online’ domain with our hosting packages starting from the Plus Plan and above. This means that when you choose one of these plans, you’ll receive a free ‘.online’ domain included.`,
+            Img: ``
+        },
+        {
+            Title: `User-friendly Tutorials`,
+            Text: `At Ducehost, we are dedicated to ensuring that you have the resources you need to succeed. Our user-friendly tutorials and self-help guides are here to empower you on your digital journey.`,
+            Img: ``
+        },
+    ],
+    ppSectionTitle: `Pricing Plans`,
+    ppSectionText: `If you have the vision for a website, blog, or online store, Ducehost has the perfect hosting package to turn your vision into reality. We cater to your every need.`,
+    testimonySectionTitle: `Testimonials`,
+    testimonySectionText: `Don’t just take our word for it, hear what our satisfied customers have to say`,
+    FAQs: {
+        questionOne: `What is Web hosting and why do i need it?`,
+        answerOne: ``,
+        questionTwo: `How can i register a domain name with Ducehost?`,
+        answerTwo: ``,
+        questionThree: `What kind of customer support do you offer?`,
+        answerThree: ``,
+        questionFour: `How do i migrate to Ducehost?`,
+        answerFour: ``,
+        questionFive: `Which hosting plan is marked as recommended for WordPress?`,
+        answerFive: ``,
+    }
+};
