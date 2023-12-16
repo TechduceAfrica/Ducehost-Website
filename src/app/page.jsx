fef9190f194d744d9c0ab03d97b69284b28def0a
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-12-16 14:20:55 +0100

    Home page created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,65 +1,78 @@
-"use client"
-import Image from 'next/image';
-import styles from './page.module.css';
+'use client'
+import ImgBGHeroSection from '@/components/ImgBGHeroSection';
+import style from './page.module.css'
 import { useEffect } from 'react';
-import SubText from '@/components/SubText';
-import HeadlineSmall from '@/components/HeadlineSmall';
-import SecBtn from '@/components/SecBtn';
-import PriBtn from '@/components/PriBtn';
-import PriBtnWine from '@/components/PriBtnWine';
-import DomainSearchBar from '@/components/DomainSearchBar';
+import { FAQsCopy, homeCopy } from '@/copyContents/HomePage';
+import RightLeftLayout from '@/components/RightLeftLayout';
+import HostingPriceCard from '@/components/HostingPriceCard';
+import { Hosting } from '@/copyContents/PricePlan';
+import FAQs from '@/components/FAQs';
+import Testimonial from '@/components/Testimonial';
 
 export default function Home() {
 
-  useEffect( () => {
-    (
-      async () => {
-        const LocomotiveScroll = (await import('locomotive-scroll')).default;
-        const locomotiveScroll = new LocomotiveScroll();
-      }
-    )()
-  }, [])
+    useEffect( () => {
+        (
+          async () => {
+            const LocomotiveScroll = (await import('locomotive-scroll')).default;
+            const locomotiveScroll = new LocomotiveScroll();
+          }
+        )()
+    }, [])
 
-  return (
-    <main className={styles.main}>
-      <div>
-        <DomainSearchBar/>
-      </div>
-      <div>
-        <h1>H1 One</h1>
-        <h2>H2 Two</h2>
-        <h3>H3 Three</h3>
-        <h4>H4 Four</h4>
-        <h5>H5 Five</h5>
-        <h6>H6 Six</h6>
-        <p>Paragraph</p>
-        <SubText
-          text='Sub text font'
-          wine= 'with the wine colored font'
-          textContiue= 'and then black again'
-        />
-        <HeadlineSmall
-          text= 'Headline small font' 
-          wine= 'with the wine colored font'
-          textContiue= 'and then black again'
-        />
-      </div>
-      <div>
-        <SecBtn
-          SecBtnText= 'Sec Button'
-          SecBtnLink= '/'
-        />
-
-        <PriBtn
-          PriBtnText= 'Pri Button'
-          PriBtnLink= '/'
-        />
-
-        <PriBtnWine
-          PriBtnWineText= 'Wine Button'
-          PriBtnWineLink= '/'
-        />
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <ImgBGHeroSection
+                HeroSecBgImgLink={homeCopy.heroBG}
+                text={homeCopy.heroIntro}
+                title={homeCopy.heroTitle}
+            />
+            <section className='container-width'>
+                <div className={style.home__sec}>
+                    <div className={style.content__copy}>
+                        <h3>
+                            {homeCopy.wcuSectionTitle}
+                        </h3>
+                        <p>
+                            {homeCopy.wcuSectionSubTitle}
+                        </p>
+                    </div>
+                    <RightLeftLayout
+                        rlLayout={homeCopy.wcuList}
+                    />
+                </div>
+                <div className={style.home__sec}>
+                    <div className={style.content__copy}>
+                        <h3>
+                            {homeCopy.ppSectionTitle}
+                        </h3>
+                        <p>
+                            {homeCopy.ppSectionText}
+                        </p>
+                    </div>
+                    <HostingPriceCard
+                        hostingPlans={Hosting.plans}
+                    />
+                </div>
+                <div className={style.home__sec}>
+                    <div className={style.content__copy}>
+                        <h3>
+                            {homeCopy.testimonySectionTitle}
+                        </h3>
+                        <p>
+                            {homeCopy.testimonySectionText}
+                        </p>
+                    </div>
+                    <div>
+                        <Testimonial/>
+                    </div>
+                </div>
+                <div className={style.home__sec}>
+                    <FAQs
+                        faqsContent={FAQsCopy}
+                    />
+                </div>
+            </section>
+        </main>
+    )
 }
