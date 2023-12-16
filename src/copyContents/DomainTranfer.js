bb27ab6032caaf1fd3e8e767bbc6b14d35ecf92e
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-12-16 14:22:50 +0100

    Update: Content copy update

---
+++ src/copyContents/DomainTranfer.js
@@ -0,0 +1,23 @@
+export const whyTransfer = [
+    {
+        imgLink: `/images/domain-transfer_consolidation.webp`,
+        width: `120`,
+        height: `120`,
+        title: `Consolidation`,
+        description: `Keep your website and domain under one roof for easier management and centralized support.`,
+    },
+    {
+        imgLink: `/images/competitive-pricing.webp`,
+        width: `120`,
+        height: `120`,
+        title: `Competitive Pricing`,
+        description: `Benefit from our competitive domain pricing and renewal rates.`,
+    },
+    {
+        imgLink: `/images/reliable-hosting.webp`,
+        width: `120`,
+        height: `120`,
+        title: `Reliable Hosting`,
+        description: `Pair your domain with our reliable hosting services, enhancing your website's performance and security.`,
+    },
+]
