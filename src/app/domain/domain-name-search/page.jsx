df926230c6d6c71a917c3370706b0a7beba518aa
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-12-17 23:27:37 +0100

    Page: Domain search page done

---
+++ src/app/domain/domain-name-search/page.jsx
@@ -0,0 +1,56 @@
+'use client'
+import ImgBGHeroSection from '@/components/ImgBGHeroSection';
+import style from './page.module.css';
+import { useEffect } from 'react';
+import { HeroSection, wcdSection } from '@/copyContents/DomainSearchPageCopy';
+import TLDPriceCards from '@/components/TLDPriceCards';
+import WcuSection from '@/components/WcdSection';
+import DiscoverDomain from '@/components/DiscoverDomain';
+import OtherDomainNeeds from '@/components/OtherDomainNeeds';
+
+export default function DomainNameSearch() {
+
+    useEffect( () => {
+        (
+          async () => {
+            const LocomotiveScroll = (await import('locomotive-scroll')).default;
+            const locomotiveScroll = new LocomotiveScroll();
+          }
+        )()
+    }, [])
+
+
+    return (
+        <main>
+            <ImgBGHeroSection
+                HeroSecBgImgLink={HeroSection.heroBG}
+                text={HeroSection.heroIntro}
+                title={HeroSection.heroTitle}
+            />
+            <div className='container__width'>
+                <section className={style.tld__card__sec}>
+                    <div className={style.tld__card__content}>
+                        <h3>
+                            Top Level Domain (TLD) Spotlight
+                        </h3>
+                        <p>
+                            Find the perfect address for your online venture.
+                        </p>
+                    </div>
+                    <div>
+                        <TLDPriceCards/>
+                    </div>
+                </section>
+                <section>
+                    <WcuSection/>
+                </section>
+            </div>
+            <section>
+                <DiscoverDomain/>
+            </section>
+            <section>
+                <OtherDomainNeeds/>
+            </section>
+        </main>
+    )
+}
