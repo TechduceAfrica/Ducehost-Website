c001a48ecb22d54eb4ab0de414d637db91692854
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-11 23:36:58 +0100

    Create StepTwo.jsx

---
+++ src/components/DomainTransferGuide/StepTwo.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import style from './domaintransferguide.module.css';
+import Image from 'next/image';
+
+export default function StepTwo({ step, title, imgLink, width, height }) {
+    return (
+        <>
+            <div className={style.step__img__wrapper}>
+                <Image
+                    src={imgLink}
+                    alt={title}
+                    title={title}
+                    width={width}
+                    height={height}
+                />
+            </div>
+            <div className={style.step__copy__wrapper}>
+                <span className={`${style.step__number} wine`}>
+                    {step}
+                </span>
+                <h4>
+                    {title}
+                </h4>
+                <ul>
+                    <li>
+                        Request the domain name’s authorization code (also known as the 
+                        <a href='/epp-code' target='_blank' title='EPP Code Link' className='wine'> EPP code </a> 
+                        or transfer code) from your current registrar.
+                    </li>
+                </ul>
+
+            </div>
+        </>
+    )
+}
