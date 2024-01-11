82cc126e45e816e645b0d1ddcb3c0076af73e7c1
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-11 23:37:11 +0100

    Create index.jsx

---
+++ src/components/DomainTransferGuide/index.jsx
@@ -0,0 +1,34 @@
+import StepOne from './StepOne';
+import StepTwo from './StepTwo';
+import style from './domaintransferguide.module.css';
+import { DomainTransferGuide as Copy } from '@/copyContents/DomainTranfer';
+
+export default function DomainTransferGuide() {
+
+    return (
+        <>
+            <div className={`${style.step__topic__wrapper} margin__top__80`}>
+                <h3>
+                    Step-by-Step <span className='wine'> Transfer </span>Guide
+                </h3>
+                <p>
+                    Transferring your domain is <span className='wine'> simple</span>, and we are here to guide you through the process. 
+                </p>
+            </div>
+            <div className={style.steps__sec}>
+                <div className={style.step__sec__wrapper}>
+                    <StepOne {...Copy.stepOne}/>
+                </div>
+                <div className={style.step__sec__wrapper}>
+                    <StepTwo {...Copy.stepTwo}/>
+                </div>
+                <div className={style.step__sec__wrapper}>
+                    <StepOne {...Copy.stepThree}/>
+                </div>
+                <div className={style.step__sec__wrapper}>
+                    <StepOne {...Copy.stepFour}/>
+                </div>
+            </div>
+        </>
+    )
+}
