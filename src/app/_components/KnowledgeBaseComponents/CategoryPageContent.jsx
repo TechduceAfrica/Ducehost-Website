e1083c8f2fe15016dbc41435643a391a04055c67
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-24 23:19:09 +0100

    create: created category page for the knowledge base articles

---
+++ src/app/_components/KnowledgeBaseComponents/CategoryPageContent.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+import style from './knowledgebase.module.css'
+
+export default function CategoryPageContent({ title, articles, category }) {
+    return (
+        <article>
+            <h2>
+                <Link href={"/help/knowledge-base/"}>Knowledge Base</Link> / {title}
+            </h2>
+            <div className={style.sub__category__screen}>
+                {articles.map((article) => (
+                    <Link 
+                        key={article.id} 
+                        href="/help/knowledge-base/[category]/[article]" 
+                        as={`/help/knowledge-base/${category.slug}/${article.link}`} 
+                        target='_blank'
+                    >
+                        <div className={style.sub__category__card}>
+                            {article.topic}
+                        </div>
+                        <span>
+                            1 article
+                        </span>
+                    </Link>
+                )
+                )}
+            </div>
+        </article>
+    )
+}
