2c7fb55cd5667c9d9c7526c51bfe093ebfc461d0
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-30 17:41:38 +0100

    refactor

--- src/app/_components/UseWhois/MapUseWhoisCard.jsx
+++ src/app/_components/UseWhois/MapUseWhoisCard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 export default function MapUseWhoisCard() {
-  return (
-    <div>MapUseWhoisCard</div>
-  )
+    return (
+        <div>
+          
+        </div>
+    )
 }
