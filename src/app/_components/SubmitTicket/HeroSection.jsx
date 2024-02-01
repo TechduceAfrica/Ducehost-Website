06686875ebbbcccd9d93d27694bb58ea8ba6fcbf
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2024-02-01 06:45:16 -0800

    chore(submit-ticket): Form Layout, Validation and Data-Capturing

--- src/app/_components/SubmitTicket/HeroSection.jsx
+++ src/app/_components/SubmitTicket/HeroSection.jsx
@@ -1,33 +1,288 @@
-import React from 'react'
-import Image from 'next/image'
-import style from '../ReportAbuse/ReportAbuse.module.css'
-import { heroDataCopy } from '@/copyContents/SubmitTicketCopy'
+"use client";
 
-export default function HeroSection() {
-    return (
-        <section className={`${style.hero__section} container__width`}>
+import { React, useState } from "react";
+import Image from "next/image";
+import style from "../ReportAbuse/ReportAbuse.module.css";
+import { heroDataCopy } from "@/copyContents/SubmitTicketCopy";
+import PriBtn from "../PriBtn";
+import { motion } from "framer-motion";
+
+import SectionWrapper from "@/app/hoc/SectionWrapper";
+import { fadeIn } from "@/app/utils/motion";
+
+const HeroSection = () => {
+  const [loading, setLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formErrors, setFormErrors] = useState({
+    name: false,
+    email: false,
+    subject: false,
+    message: false,
+  });
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+    subject: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+    setFormErrors({ ...formErrors, [name]: false });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const isFormValid = validateForm();
+
+    if (isFormValid) {
+      setLoading(true);
+      const date = new Date();
+      const timeStamp = date;
+
+      const data = {
+        formData: {
+          name: form.name,
+          email: form.email,
+          subject: form.subject,
+          message: form.message,
+        },
+        timeStamp,
+      };
+
+      console.log(data);
+
+      // try {
+      //   // const uploadToDatabase = async (data) => {
+      //   //   // Add a new document in collection ""
+
+      //   //   await setDoc(doc(db, "users", data.formData.email), {
+      //   //     name: data.formData.name,
+      //   //     email: data.formData.email,
+      //   //     message: data.formData.message,
+      //   //     subject: data.formData.subject,
+      //   //     timeStamp: data.timeStamp,
+      //   //   });
+      //   // };
+      //   // await uploadToDatabase(data);
+
+      //   // Send data to  API
+      //   const postData = async (data) => {
+      //     try {
+      //       const response = await fetch("/api/send", {
+      //         method: "POST",
+      //         headers: {
+      //           "Content-Type": "application/json",
+      //           Accept: "application/json",
+      //         },
+      //         body: JSON.stringify(data),
+      //       });
+
+      //       const result = await response.json();
+
+      //       if (!response.ok) {
+      //         throw new Error("Failed to send data");
+      //       }
+
+      //       console.log(result.status);
+      //       return result;
+      //     } catch (error) {
+      //       console.error(error);
+      //       // You can handle the error here or throw it further if needed
+      //       throw error;
+      //     }
+      //   };
+
+      //   const response = await postData(data);
+      //   console.log(response.status);
+
+      //   setLoading(false);
+
+      //   if (response.status === "undefined") {
+      //     setIsSubmitted(true);
+      //     toast.success(` Congrats ${form.name}!, Your E-book is Downloading!`);
+      //     resetForm();
+      //   } else {
+      //     // Handle the case when the API call fails
+
+      //     setIsSubmitted(true);
+      //     toast.success(` Congrats ${form.name}!, Your E-book is Downloading!`);
+      //     resetForm();
+      //   }
+      // } catch (error) {
+      //   // Handle errors that may occur during database or API operations
+      //   // alert("An Error occured, please try again " + error.message);
+      //   // setLoading(false);
+      //   // resetForm();
+
+      //   // Log or display an error message to the user
+      //   console.error(error);
+      // }
+    }
+  };
+
+  const validateForm = () => {
+    const errors = {
+      name: form.name === "",
+      email: form.email === "",
+      subject: form.subject === "",
+      message: form.message === "",
+    };
+
+    setFormErrors(errors);
+
+    return Object.values(errors).every((error) => !error);
+  };
+
+  const resetForm = () => {
+    setForm({
+      name: "",
+      email: "",
+      message: "",
+      subject: "",
+    });
+  };
+
+  return (
+    <section className={`${style.hero__section} container__width`}>
+      <div>
+        <div>
+          <h1 dangerouslySetInnerHTML={{ __html: heroDataCopy.title }}></h1>
+          <p>{heroDataCopy.body}</p>
+        </div>
+        <div>
+          <Image
+            src={heroDataCopy.img}
+            alt={heroDataCopy.alt}
+            title={heroDataCopy.alt}
+            width={heroDataCopy.width}
+            height={heroDataCopy.height}
+          />
+        </div>
+      </div>
+      <motion.div variants={fadeIn("left", "spring", 0.2, 0.75)}>
+        <p>{heroDataCopy.formDesc}</p>
+
+        <form className="form  py-[20px]" onSubmit={handleSubmit}>
+          <div className="gap-[30px] flex flex-col">
+            {/* NAME */}
             <div>
-                <div>
-                    <h1 dangerouslySetInnerHTML={{ __html: heroDataCopy.title }}></h1>
-                    <p>
-                        {heroDataCopy.body}
-                    </p>
+              <label htmlFor="name" className="text-semibold text-[16px]">
+                Name
+              </label>
+              <div
+                className={`w-[100%] border-2 ${
+                  formErrors.name ? "border-red-600" : "border-primary-blue"
+                } rounded-[5px] flex flex-row justify-between`}
+              >
+                <input
+                  id="name"
+                  name="name"
+                  className="w-full py-3 px-2"
+                  type="text"
+                  value={form.name}
+                  placeholder="What's your name?"
+                  onChange={handleChange}
+                />
+              </div>
+              {formErrors.name && (
+                <div className="text-red-600 text-[14px] ">
+                  Name is required
                 </div>
-                <div>
-                    <Image 
-                        src={heroDataCopy.img} 
-                        alt={heroDataCopy.alt} 
-                        title={heroDataCopy.alt} 
-                        width={heroDataCopy.width} 
-                        height={heroDataCopy.height}
-                    />
-                </div>
+              )}
             </div>
+
+            {/* Email */}
             <div>
-                <p>
-                    {heroDataCopy.formDesc}
-                </p>
+              <label htmlFor="email" className="text-semibold text-[16px]">
+                Email
+              </label>
+              <div
+                className={`w-[100%] border-2 ${
+                  formErrors.email ? "border-red-600" : "border-primary-blue"
+                } rounded-[5px] flex flex-row justify-between`}
+              >
+                <input
+                  id="email"
+                  name="email"
+                  className="w-full py-3 px-2"
+                  type="email"
+                  value={form.email}
+                  placeholder="Your E-mail Address?"
+                  onChange={handleChange}
+                />
+              </div>
+              {formErrors.email && (
+                <div className="text-red-600 text-[14px] ">
+                  Email is required
+                </div>
+              )}
             </div>
-        </section>
-    )
-}
+
+            {/* Subject */}
+            <div>
+              <label htmlFor="subject" className="text-semibold text-[16px]">
+                Subject
+              </label>
+              <div
+                className={`w-[100%] border-2 ${
+                  formErrors.subject ? "border-red-600" : "border-primary-blue"
+                } rounded-[5px] flex flex-row justify-between`}
+              >
+                <input
+                  id="subject"
+                  name="subject"
+                  className="w-full py-3 px-2"
+                  type="tel"
+                  placeholder="Subject"
+                  value={form.subject}
+                  onChange={handleChange}
+                />
+              </div>
+              {formErrors.subject && (
+                <div className="text-red-600 text-[14px] ">
+                  Subject is required
+                </div>
+              )}
+            </div>
+
+            {/* MESSAGES */}
+            <div>
+              <label htmlFor="message" className="text-semibold text-[16px]">
+                Message
+              </label>
+              <div
+                className={`w-[100%] border-2 ${
+                  formErrors.message ? "border-red-600" : "border-primary-blue"
+                } rounded-[5px] flex flex-row justify-between`}
+              >
+                <textarea
+                  id="message"
+                  name="message"
+                  className="w-full py-3 px-2"
+                  rows={3}
+                  value={form.message}
+                  placeholder="How can we help you?"
+                  onChange={handleChange}
+                />
+              </div>
+              {formErrors.message && (
+                <div className="text-red-600 text-[14px] ">
+                  Message is required
+                </div>
+              )}
+            </div>
+            <div className="font-semibold">
+              <PriBtn PriBtnText={loading ? "Submitting..." : "Submit"} />
+            </div>
+          </div>
+        </form>
+      </motion.div>
+    </section>
+  );
+};
+
+export default SectionWrapper(HeroSection, "");
