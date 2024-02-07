e5d2a98925b67c5d01b579d7d7d572a4aa4fc5d5
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-02-07 15:23:28 +0100

    Create WithPriBtn.jsx

---
+++ src/app/_components/ImgBGHeroSection/WithPriBtn.jsx
@@ -0,0 +1,40 @@
+'use client'
+import PriBtn from '../PriBtn';
+import style from './component.module.css';
+
+export default function WithPriBtn({ BgImgLink, text, title, btnLink, btnText }) {
+
+    const styles = {
+        background: `url(${BgImgLink})`,
+        backgroundSize: 'cover',
+    }
+
+    return (
+        <section 
+            style={styles} 
+            className={style.hero__section}
+        >
+            <p 
+                className={style.hero__section__intro__text} 
+                data-scroll data-scroll-speed="0.15"
+            >
+                {text}
+            </p>
+            <h1 
+                className={style.hero__section__title} 
+                data-scroll data-scroll-speed="0.1"
+            >
+                {title}
+            </h1>
+            <div 
+                className={style.hero__center__btn} 
+                data-scroll data-scroll-speed="0.05"
+            >
+                <PriBtn 
+                    PriBtnLink={btnLink} 
+                    PriBtnText={btnText}
+                />
+            </div>
+        </section>
+    )
+}
