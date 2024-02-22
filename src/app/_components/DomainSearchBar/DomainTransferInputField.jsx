64172f3f4676a2fe6d01c74b2ba9a5452d70eacd
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-02-22 10:13:15 +0100

    function: Added the domain search and transfer functions

--- src/app/_components/DomainSearchBar/DomainTransferInputField.jsx
+++ src/app/_components/DomainSearchBar/DomainTransferInputField.jsx
@@ -3,23 +3,20 @@ import style from './component.module.css'
 
 export default function DomainTransferInputField() {
     return (
-        <form className={style.domain__name__single__search__form}>
+        <form 
+            action="https://www.ducehost.com/host/cart.php?a=add&domain=transfer" 
+            method="post"
+            className={style.domain__name__single__search__form}
+        >
             <div className={style.domain__name__search__bar}>
                 <input 
                     type="text" 
-                    name="Domain Transfer" 
-                    value={''} 
-                    onChange={(e) => setDomain(e.target.value)}
+                    name="query" 
                     placeholder="Enter Domain name you want to transfer" 
                 /> 
-                <button 
-                    // onClick={''}
-                >
+                <button>
                     Transfer
                 </button>
-            </div>
-            <div>
-                {/* {status && <p>Domain name status: {status}</p>} */}
             </div>
         </form>
     )
--- src/app/_components/DomainSearchBar/index.jsx
+++ src/app/_components/DomainSearchBar/index.jsx
@@ -3,22 +3,25 @@ import style from './component.module.css'
 export default function DomainSearchBar() {
 
     return (
-        <form className={style.domain__name__search__form} data-scroll data-scroll-speed="0.1">
+        <form 
+            action="https://ducehost.com/host/cart.php?a=add&domain=register" 
+            method="post"
+            className={style.domain__name__search__form} 
+            data-scroll 
+            data-scroll-speed="0.1"
+        >
             <div className={style.domain__name__search__bar}>
                 <input 
                     type="text" 
-                    name="Domain search" 
-                    value={''} 
-                    onChange={(e) => setDomain(e.target.value)}
+                    name="query" 
+                    size="20"
                     placeholder="Enter your domian name" 
                 />
-                <button 
-                    // onClick={''}
-                >
+                <button>
                     Search
                 </button>
             </div>
-            <div className={style.tld__list}>
+            {/* <div className={style.tld__list}>
                 <p>
                     .com <span>| ₦8500</span>
                 </p>
@@ -31,10 +34,7 @@ export default function DomainSearchBar() {
                 <p>
                     .biz <span>| ₦6000</span>
                 </p>
-            </div>
-            <div>
-                {/* {status && <p>Domain name status: {status}</p>} */}
-            </div>
+            </div> */}
         </form>
     )
 }
